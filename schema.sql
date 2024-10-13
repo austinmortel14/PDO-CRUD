df18a144a574968ad8faec5a2fcb9bbaec84856b
austinmortel14 <167597240+austinmortel14@users.noreply.github.com>
2024-10-13 22:18:38 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,21 +1,10 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-CREATE TABLE `CarFormance` (
-  `CarCustomerID` int(11) NOT NULL,             -- Customer ID 
-  `CustomerName` varchar(100) DEFAULT NULL,     -- Customer name()
-  `CarModel` varchar(100) DEFAULT NULL,         -- Car Make and Model
-  `Mechanic` varchar(100) DEFAULT NULL,         -- Assigned Mechanic
-  `ServiceAdviser` varchar(100) DEFAULT NULL,   -- Assigned Service Adviser
-  `ServiceType` varchar(100) DEFAULT NULL,      -- Type of service being done 
-  `NextMaintenance` varchar(100) DEFAULT NULL,  -- Next maintenance schedule
-  `Clearance` tinyint(1) DEFAULT NULL,          -- Clearance for vehicle exit
-  `ServiceDate` timestamp NOT NULL DEFAULT current_timestamp()    -- Current Service Date for vehicle 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `CarFormance`
-  ADD PRIMARY KEY (`CarCustomerID`);
-  
-  ALTER TABLE `CarFormance`
-  MODIFY `CarCustomerID` int(11) NOT NULL AUTO_INCREMENT;
+CREATE TABLE customer_records (
+    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerName VARCHAR(255) NOT NULL,
+    CustomerCarModel VARCHAR(255) NOT NULL,
+    MechanicAssigned VARCHAR(255) NOT NULL,
+    ServiceAdviserAssigned VARCHAR(255) NOT NULL,
+    ServiceType VARCHAR(255) NOT NULL,
+    CurrentServiceDate DATE NOT NULL,
+    NextMaintenanceDate DATE NOT NULL
+);
