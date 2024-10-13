dbe0386539249d5b6f87a6d94ff735d6cc2f9cb8
austinmortel14 <167597240+austinmortel14@users.noreply.github.com>
2024-10-13 22:17:55 +0800

    Update handlesform.php

--- handlesform.php
+++ handlesform.php
@@ -1,83 +1,18 @@
-<?php 
+<?php
+include 'dbconfig.php';
 
-require_once 'dbConfig.php'; 
-require_once 'models.php';
+if (isset($_POST['CustomerID'])) {
+    $id = $_POST['CustomerID'];
+    $name = $_POST['CustomerName'];
+    $carModel = $_POST['CustomerCarModel'];
+    $mechanic = $_POST['MechanicAssigned'];
+    $advisor = $_POST['ServiceAdviserAssigned'];
+    $serviceType = $_POST['ServiceType'];
+    $currentDate = $_POST['CurrentServiceDate'];
+    $nextDate = $_POST['NextMaintenanceDate'];
 
-if (isset($_POST['insertNewStudentBtn'])) {
-	$firstName = trim($_POST['firstName']);
-	$lastName = trim($_POST['lastName']);
-	$gender = trim($_POST['gender']);
-	$yearLevel = trim($_POST['yearLevel']);
-	$section = trim($_POST['section']);
-	$adviser = trim($_POST['adviser']);
-	$religion = trim($_POST['religion']);
+    $stmt = $pdo->prepare('UPDATE customer_records SET CustomerName = ?, CustomerCarModel = ?, MechanicAssigned = ?, ServiceAdviserAssigned = ?, ServiceType = ?, CurrentServiceDate = ?, NextMaintenanceDate = ? WHERE CustomerID = ?');
+    $stmt->execute([$name, $carModel, $mechanic, $advisor, $serviceType, $currentDate, $nextDate, $id]);
 
-	if (!empty($firstName) && !empty($lastName) && !empty($gender) && !empty($yearLevel) && !empty($section)  && !empty($section)  && !empty($adviser) && !empty($religion)) {
-			$query = insertIntoStudentRecords($pdo, $firstName, $lastName, 
-			$gender, $yearLevel, $section, $adviser, $religion);
-
-		if ($query) {
-			header("Location: ../index.php");
-		}
-
-		else {
-			echo "Insertion failed";
-		}
-	}
-
-	else {
-		echo "Make sure that no fields are empty";
-	}
-	
+    header('Location: index.php');
 }
-
-
-if (isset($_POST['editStudentBtn'])) {
-	$student_id = $_GET['student_id'];
-	$firstName = trim($_POST['firstName']);
-	$lastName = trim($_POST['lastName']);
-	$gender = trim($_POST['gender']);
-	$yearLevel = trim($_POST['yearLevel']);
-	$section = trim($_POST['section']);
-	$adviser = trim($_POST['adviser']);
-	$religion = trim($_POST['religion']);
-
-	if (!empty($student_id) && !empty($firstName) && !empty($lastName) && !empty($gender) && !empty($yearLevel) && !empty($section) && !empty($adviser) && !empty($religion)) {
-
-		$query = updateAStudent($pdo, $student_id, $firstName, $lastName, $gender, $yearLevel, $section, $adviser, $religion);
-
-		if ($query) {
-			header("Location: ../index.php");
-		}
-		else {
-			echo "Update failed";
-		}
-
-	}
-
-	else {
-		echo "Make sure that no fields are empty";
-	}
-
-}
-
-
-
-
-
-if (isset($_POST['deleteStudentBtn'])) {
-
-	$query = deleteAStudent($pdo, $_GET['student_id']);
-
-	if ($query) {
-		header("Location: ../index.php");
-	}
-	else {
-		echo "Deletion failed";
-	}
-}
-
-
-
-
-?>
